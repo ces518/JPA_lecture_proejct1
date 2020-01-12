1b0c0fd29516e6282fbf5a1c62a7e93f2264bbe7
ces518 <pupupee9@gmail.com>
2020-01-12 22:59:10 +0900

    프로젝트 생성

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: june
+ * Date: 12/01/2020
+ * Time: 10:55 오후
+ **/
+@Getter @Setter
+public class Hello {
+    private String data;
+}
