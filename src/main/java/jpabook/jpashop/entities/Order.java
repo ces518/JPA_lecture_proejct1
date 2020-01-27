31f76f577d28321501a04ea7ad80e6eab016ab7f
ces518 <pupupee9@gmail.com>
2020-01-27 20:58:25 +0900

    주문 도메인 개발 - 주문 & 주문상품 엔티티 개발(비즈니스로직 추가)

--- src/main/java/jpabook/jpashop/entities/Order.java
+++ src/main/java/jpabook/jpashop/entities/Order.java
@@ -60,4 +60,66 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //== 생성 메소드 ==//
+    // 생성 메소드를 사용하는것이 중요함
+    // 여기저기 비즈니스로직을 찾아다니지 않아도되고, 변경이 일어나더라도 한곳에서 관리가 된다.
+
+    /**
+     * 주문생성에 대한 비즈니스로직을 응집해 두는것
+     */
+    public static Order createOrder (Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        // 주문자 세팅
+        order.setMember(member);
+        // 딜리버리 세팅
+        order.setDelivery(delivery);
+
+        // 주문상품 세팅
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        // 주문상태: 주문으로 세팅
+        order.setStatus(OrderStatus.ORDER);
+        // 주문시간을 현재로 세팅
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //== 비즈니스 로직 ==//
+    /**
+     * 주문 취소
+     * - 배송완료된 상품은 취소하지 못하다는 비즈니스로직
+     */
+    public void cancel () {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소할 수 없습니다.");
+        }
+        // 상태를 취소로 변경
+        this.setStatus(OrderStatus.CANCEL);
+        // 상품의 재고를 원복한다.
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //== 조회 로직 ==//
+    // 뭔가 계산이 필요할때..
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice () {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+        /*
+        int totalPrice = 0;
+
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+        */
+    }
 }
--- src/main/java/jpabook/jpashop/entities/OrderItem.java
+++ src/main/java/jpabook/jpashop/entities/OrderItem.java
@@ -31,4 +31,36 @@ public class OrderItem {
     private int orderPrice; // 주문가격 (주문당시 가격)
 
     private int count; // 수량
+
+    //== 생성 메서드 ==//
+    public static OrderItem createOrderItem (Item item, int orderPrice, int count) {
+        // orderPrice를 Item의 price를 참조하지않고 따로 받아오는 이유는 할인을 하는등의 상황등에 대처하기 위함
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        // 구매하는것 만큼 재고를 감소시킨다.
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    //== 비즈니스 로직 ==//
+    /**
+     * 재고수량을 원복한다.
+     */
+    public void cancel() {
+        // 재고수량을 원복한다.
+        getItem().addStock(this.count);
+    }
+
+    //== 조회 로직 ==//
+
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
