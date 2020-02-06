151119236395a32bb1720f08e41cbc4b07b290d4
ces518 <pupupee9@gmail.com>
2020-02-06 23:08:50 +0900

    웹계층 개발 - 상품 수정

--- src/main/java/jpabook/jpashop/web/ItemController.java
+++ src/main/java/jpabook/jpashop/web/ItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -50,5 +52,37 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm (@PathVariable Long itemId, Model model) {
+        // 캐스팅하는것이 좋지는 않지만 예제 단순화를 위해 캐스팅함
+        Book findBook = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(findBook.getId());
+        form.setName(findBook.getName());
+        form.setPrice(findBook.getPrice());
+        form.setStockQuantity(findBook.getStockQuantity());
+        form.setAuthor(findBook.getAuthor());
+        form.setIsbn(findBook.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem (@ModelAttribute("form") BookForm form) {
+
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
