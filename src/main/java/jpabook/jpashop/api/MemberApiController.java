79cfed32e30d28bbc357b1424a9044572627f8af
ces518 <pupupee9@gmail.com>
2020-02-07 21:34:30 +0900

    API 개발 - 회원 등록 API

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.entities.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: june
+ * Date: 2020-02-07
+ * Time: 21:08
+ **/
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1 (@RequestBody @Valid Member member) {
+        // 엔티티를 그대로 사용해선 안된다
+        // api 스펙을위한 DTO를 사용해야한다.
+        // > 엔티티가 수정되면 api스펙이 변경된다.
+        // > 엔티티를 그대로 사용했을때 발생한 장애 케이스가 매우 많다.
+        // 엔티티를 api 파라메터로 절대 쓰지말것
+        // 외부에 노출해서도 안된다.
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2 (@RequestBody @Valid CreateMemberRequest request) {
+        // DTO를 사용한 케이스
+        // 엔티티가 수정되어도 api에 영향을 받지않는다.
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
--- src/main/java/jpabook/jpashop/entities/Member.java
+++ src/main/java/jpabook/jpashop/entities/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +22,9 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    // 화면 벨리데이션에 대한 로직이 엔티티에 있으면 안된다
+    // 특정 api에선 필요하고, 또 다른 api에서는 쓰지 않을수도 있음
+//    @NotEmpty
     private String name;
 
     @Embedded
